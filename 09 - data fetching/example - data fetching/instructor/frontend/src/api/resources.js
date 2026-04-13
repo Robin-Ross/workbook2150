4b8de0f7d2a1fa65b9a041df2406e9c198835841
Oliver Antoniu <oliver+github@abolish.golf>
2026-04-13 17:30:54 -0600

    add [09 - data fetching]: ex - API function skeletons & first simple loader function

---
+++ 09 - data fetching/example - data fetching/instructor/frontend/src/api/resources.js
@@ -0,0 +1,3 @@
+export async function getResources() {}
+
+export async function getResourceById() {}
--- 09 - data fetching/example - data fetching/instructor/frontend/src/router/loaders.js
+++ 09 - data fetching/example - data fetching/instructor/frontend/src/router/loaders.js
@@ -1,3 +1,18 @@
-export async function ResourceDirectoryLoader() {}
+import { getResources, getResourceById } from '../api/resources';
+
+export async function ResourceDirectoryLoader() {
+  const resources = await getResources();
+  return { resources };
+  /* The point of a loader function is to grab some data,
+     and return an object with all of that data (might be multiple things)
+     showing up in named properties.
+
+     In the component, we'll use this like:
+       import { useLoaderData } from 'react-router';
+
+       const data = useLoaderData(); // grabs whatever data is at that route automatically
+       console.log(data.resources)
+  */
+}
 
 export async function AdminLoader() {}
