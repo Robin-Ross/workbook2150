cb319306ddffb24a6468bff1f585d57aa5485efd
Oliver Antoniu <oliver+github@abolish.golf>
2026-04-13 17:40:16 -0600

    add [09 - data fetching]: ex - add admin loader (handles both 'admin/' & 'admin/:resourceId' cases

--- 09 - data fetching/example - data fetching/instructor/frontend/src/router/loaders.js
+++ 09 - data fetching/example - data fetching/instructor/frontend/src/router/loaders.js
@@ -15,4 +15,33 @@ export async function ResourceDirectoryLoader() {
   */
 }
 
-export async function AdminLoader() {}
+export async function AdminLoader({ params }) {
+  // params is a built-in keyword for URL params that react-router loader functions can use
+
+  // 1. we still need the entire list of resources
+  const resources = await getResources();
+
+  /* 2. we also want a by-ID fetch/loader:
+        - make sure we're not loading sta`le data
+        - replicate norms in REST API design where list results usually only have
+          summary info, and you have to query specific items for full details
+  */
+
+  if (!params.resourceId) { // here's us consuming our route info via params named argument
+    // first, handle the case where we're at '/admin/', looking to creat a new resource
+    return {
+      resources,
+      resourceId: null,
+      selectedResource: null,
+    }
+  }
+
+  // otherwise, try/assume that we're editing an existing one
+  const selectedResource = await getResourceById(params.resourceId);
+
+  return {
+    resources,
+    resourceId: params.resourceId,
+    selectedResource,
+  }
+}
