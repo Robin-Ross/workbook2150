e01ec39c52f8dd37f01eb6ae765236d729db59b1
Robin Ross <whytho@keemail.me>
2026-04-20 18:35:04 -0600

    (broken) Removed redundant lines

--- 10 - shared state/examples/01 - client-side/student/frontend/src/components/layout/PageLayout.jsx
+++ 10 - shared state/examples/01 - client-side/student/frontend/src/components/layout/PageLayout.jsx
@@ -1,7 +1,6 @@
 import { useContext } from 'react';
 
 import { ThemeContext } from '../../context/ThemeContext';
-import { ThemeProvider } from '../../context/ThemeProvider';
 
 export default function PageLayout({ header, children }) {
 
@@ -9,7 +8,7 @@ export default function PageLayout({ header, children }) {
 
   return (
     <div data-theme={theme} className="min-h-screen bg-base-200">
-      <header className="border-b border-sky-600 bg-white px-6 py-4">
+      <header className="border-b border-sky-600 px-6 py-4">
         {header}
       </header>
 
